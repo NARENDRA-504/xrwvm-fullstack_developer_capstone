8ba99bce90d1822edff5a09f4a229e18c1290f38
NARENDRA-504 <Narendranani92356@gmail.com>
2024-06-22 12:42:52 +0530

    Update app.js

--- server/database/app.js
+++ server/database/app.js
@@ -35,7 +35,6 @@ try {
 app.get('/', async (req, res) => {
     res.send("Welcome to the Mongoose API")
 });
-
 // Express route to fetch all reviews
 app.get('/fetchReviews', async (req, res) => {
   try {
@@ -44,6 +43,49 @@ app.get('/fetchReviews', async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: 'Error fetching documents' });
   }
+});
+
+// Express route to fetch reviews by a particular dealer
+app.get('/fetchReviews/dealer/:id', async (req, res) => {
+  try {
+    const documents = await Reviews.find({dealership: req.params.id});
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
+});
+
+// Express route to fetch all dealerships
+app.get('/fetchDealers', async (req, res) => {
+//Write your code here
+try {
+      const documents = await Dealerships.find();
+      res.json(documents);
+  } catch (error) {
+      res.status(500).json({ error: 'Error fetching documents' });
+  }	
+});
+
+// Express route to fetch Dealers by a particular state
+app.get('/fetchDealers/:state', async (req, res) => {
+//Write your code here
+ try {
+      const documents = await Dealerships.find({state: req.params.state});
+      res.json(documents);
+    } catch (error) {
+      res.status(500).json({ error: 'Error fetching documents' });
+    }
+});
+
+// Express route to fetch dealer by a particular id
+app.get('/fetchDealer/:id', async (req, res) => {
+//Write your code here
+try {
+      const documents = await Dealerships.find({id: req.params.id});
+      res.json(documents);
+    } catch (error) {
+      res.status(500).json({ error: 'Error fetching documents' });
+    }
 });
 
 // Express route to fetch reviews by a particular dealer
@@ -102,3 +144,5 @@ app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+ 
